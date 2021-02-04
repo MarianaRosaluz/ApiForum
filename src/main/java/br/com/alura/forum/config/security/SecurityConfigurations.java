0280cd43e1e038189058f2a337217ae6dc91bd45
Mariana Rosa Luz <marianarosaluz@yahoo.com>
2021-02-04 13:32:41 -0300

    Documentação da API com swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -57,5 +57,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Configuração de recursos estaticos(js, css,imagens, etc...)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html","/v2/api-docs","/webjars/**","/configuration/**","/swagger-resources/**");
     }
 }
